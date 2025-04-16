3647b465079a873ebac27607cb06b7a973f3d5a5
Oldranda1414 <leonardo.randacio@gmail.com>
2025-04-16 09:35:46 +0200

    add demo result file

---
+++ demo.py
@@ -0,0 +1,12 @@
+""" this is a demo """
+
+def print_chars(msg):
+    """this is a function
+
+    Args:
+        msg (String): this is a description
+    """
+    for c, i in enumerate[(msg)]:
+        print(c, i)
+
+MESSAGE = "hello world!"
